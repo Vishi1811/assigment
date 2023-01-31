a4e314ecab98ef532d11f42d6fe191d54dbd0a99
Vishi1811 <110190901+Vishi1811@users.noreply.github.com>
2023-01-31 23:39:45 +0530

    Update and rename assgn1Qn1.c to assgn1.c

---
+++ assgn1.c
@@ -0,0 +1,19 @@
+QN1
+#include<stdio.h>
+int main()
+{
+   printf("Hello Students");
+
+}
+QN2#include<stdio.h>
+int main()
+{
+   printf("Hello\nStudent");
+}
+Qn3
+#include<stdio.h>
+int main()
+{
+   printf("\"MySirG\"");
+
+}
