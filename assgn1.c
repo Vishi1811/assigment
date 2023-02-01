42e447b1ed0adc1a8fb138be123f201f06125efc
Vishi1811 <110190901+Vishi1811@users.noreply.github.com>
2023-02-01 23:16:35 +0530

    Update assgn1.c

--- assgn1.c
+++ assgn1.c
@@ -3,17 +3,62 @@ QN1
 int main()
 {
    printf("Hello Students");
-
+   return 0;
 }
-QN2#include<stdio.h>
+QN2 
+#include<stdio.h>
 int main()
 {
    printf("Hello\nStudent");
+   return 0;
 }
 Qn3
 #include<stdio.h>
 int main()
 {
    printf("\"MySirG\"");
-
+   return 0;
 }
+Qn4.
+#include<stdio.h>
+int main()
+{
+    float A,R;
+    printf("Enter The Radius of cicle ");
+    scanf("%f",&R);
+    A=3.14*R*R;
+    printf("Area of circle is %f having the radius %f ",A,R);
+    return 0;
+}
+Qn6.
+#include<stdio.h>
+int main()
+{
+    printf("\"Hellow\,Amit Kumar\" ");
+    return 0;
+}
+Qn7.
+#include<stdio.h>
+int main ()
+{
+   printf("%%d");
+   return 0;
+}
+Qn8.
+#include<stdio.h>
+int main ()
+{
+   printf("\\n");
+   return 0;
+}
+Qn9.
+#include<stdio.h>
+int main ()
+{
+   printf("\\\\");
+   return 0;
+}
+
+   
+
+
