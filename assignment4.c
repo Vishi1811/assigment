a4f6a553e9e42ba850a8856332c01ae261369429
Vishi1811 <110190901+Vishi1811@users.noreply.github.com>
2023-03-12 11:37:58 +0530

    Create assignment4.c

---
+++ assignment4.c
@@ -0,0 +1,130 @@
+//Qn1.#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=10;i++)
+    {N
+        printf("%d\n",i*i*i);
+    }
+    return 0;
+}
+//Qn2.
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=10;i++)
+    {
+        printf("%d\n",i);
+    }
+    return 0;
+}
+//Qn3.
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=10;i>=1;i--)
+    {
+        printf("%d\n",i);
+    }
+    return 0;
+}
+//Qn4.
+ #include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=20;i++)
+    {
+        if(i%2)
+        {
+             printf("%d\n",i);
+        }
+    }
+    return 0;
+}
+//Qn5.
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=20;i>=1;i--)
+    {
+        if(i%2)
+        {
+             printf("%d\n",i);
+        }
+    }
+    return 0;
+}
+//Qn6.
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=20;i++)
+    {
+        if(i%2==0)
+        {
+             printf("%d\n",i);
+        }
+    }
+    return 0;
+}
+//Qn7.
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=20;i>=1;i--)
+    {
+        if(i%2==0)
+        {
+             printf("%d\n",i);
+        }
+    }
+    return 0;
+}
+//Qn8.
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=10;i++)
+    {
+        printf("%d\n",i*i);
+    }
+    return 0;
+}
+//Qn9.
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=10;i++)
+    {N
+        printf("%d\n",i*i*i);
+    }
+    return 0;
+}
+//Qn10.
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1;i<=10;i++)
+    {
+        printf("%d\n",5*i);
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
+ 
